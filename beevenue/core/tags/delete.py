96c770f668c91138bb1f31d8a76be58eb292966d
Ruby <3r4o02hucyw7o9fekpx8gla2@pollination.email>
2021-07-24 14:22:02 +0200

    Move slow query from in-memory to SQL (#154)
    * Move slow query from in-memory to SQL
    
    * Run linters

--- beevenue/core/tags/delete.py
+++ beevenue/core/tags/delete.py
@@ -1,6 +1,6 @@
 from flask import g
 
-from ...models import MediaTags, Tag
+from ...models import MediaTags, Tag, MediumTagAbsence, TagAlias, TagImplication
 
 
 def delete_orphans() -> None:
@@ -10,17 +10,14 @@ def delete_orphans() -> None:
         session.query(Tag)
         .outerjoin(MediaTags)
         .filter(MediaTags.c.tag_id.is_(None))
+        .outerjoin(MediumTagAbsence)
+        .filter(MediumTagAbsence.tag_id.is_(None))
+        .outerjoin(TagAlias)
+        .filter(TagAlias.tag_id.is_(None))
+        .outerjoin(TagImplication, Tag.id == TagImplication.c.implied_tag_id)
+        .filter(TagImplication.c.implied_tag_id.is_(None))
         .all()
     )
-
-    def is_deletable(tag: Tag) -> bool:
-        return (
-            len(tag.implying_this) == 0
-            and len(tag.aliases) == 0
-            and len(tag.absent_in_media) == 0
-        )
-
-    tags_to_delete = [t for t in tags_to_delete if is_deletable(t)]
 
     for tag in tags_to_delete:
         session.delete(tag)
