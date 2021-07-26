19d73d9cd0e8f2ac0a4807b570bd790e2777851c
Ruby <3r4o02hucyw7o9fekpx8gla2@pollination.email>
2021-07-26 12:16:02 +0200

    Build deploy script (#172)
    * Fix up deploy.yml
    
    * Build up deploy.sh
    
    * Add version to debug compose file

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+set -ev -o pipefail
+
 echo "Simulate deploying now..."
-echo $1
+pwd
+docker-compose -p beevenue down
+sed -i 's/\(COMMIT_ID = \)\(.*\)/\1\"'${1:0:8}'\"/g' beevenue_config.py
+docker-compose -p beevenue up --build -d
