b1a87f4218ff561280fa4db400b3575bfed843e9
Ruby <3r4o02hucyw7o9fekpx8gla2@pollination.email>
2021-08-11 08:42:14 +0200

    Offer tag statistics (#194)

--- beevenue/core/stats_routes.py
+++ beevenue/core/stats_routes.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from flask import Blueprint
 from flask.json import jsonify
 
@@ -10,7 +12,21 @@ bp = Blueprint("stats", __name__)
 @bp.route("/stats", methods=["GET"])
 @permissions.is_owner
 def stats():  # type: ignore
-    statistics = {k: 0 for k in ["s", "q", "e", "u"]}
-    for medium in g.fast.get_all_tiny():
-        statistics[medium.rating] += 1
-    return jsonify(statistics)
+    all_media = g.fast.get_all_tiny()
+
+    rating_statistics = {k: 0 for k in ["s", "q", "e", "u"]}
+    tag_histogram = defaultdict(int)
+    absent_tag_histogram = defaultdict(int)
+
+    for medium in all_media:
+        rating_statistics[medium.rating] += 1
+        tag_histogram[len(medium.innate_tag_names)] += 1
+        absent_tag_histogram[len(medium.absent_tag_names)] += 1
+
+    return jsonify(
+        {
+            "tagHistogram": tag_histogram,
+            "absentTagHistogram": absent_tag_histogram,
+            "byRating": rating_statistics,
+        }
+    )
--- test/test_stats.py
+++ test/test_stats.py
@@ -1,5 +1,3 @@
 def test_stats(client, asAdmin):
     res = client.get("/stats")
     assert res.status_code == 200
-    result_json = res.get_json()
-    assert set(["s", "q", "e", "u"]) == set(result_json.keys())
